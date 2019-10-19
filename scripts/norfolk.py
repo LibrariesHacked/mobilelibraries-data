3c81a54aa243b69232d2a1b8c3318cca7fd1ede1
Dave <DaveBathnes@users.noreply.github.com>
2019-10-19 14:24:25 +0100

    #86 Adding norfolk routes after correcting data

--- scripts/norfolk.py
+++ scripts/norfolk.py
@@ -22,6 +22,59 @@ def read_data(data_source):
     return features
 
 def run():
+
+    CEN_Routes = {
+        "CEN117": "1",
+        "CEN118": "2",
+        "CEN220": "1",
+        "CEN222": "2",
+        "CEN223": "3",
+        "CEN319": "1",
+        "CEN320": "2",
+        "CEN323": "3",
+        "CEN421": "1",
+        "CEN422": "2",
+        "CEN423": "3",
+        "CEN521": "1",
+        "CEN522": "2",
+        "CEN523": "3",
+        "CEN113": "1",
+        "CEN213": "1",
+        "CEN214": "2",
+        "CEN215": "3",
+        "CEN313": "1",
+        "CEN314": "2",
+        "CEN321": "3",
+        "CEN413": "1",
+        "CEN414": "2",
+        "CEN415": "3",
+        "CEN513": "1",
+        "CEN514": "2",
+        "CEN515": "3",
+        "CEN114": "1",
+        "CEN115": "2",
+        "CEN216": "1",
+        "CEN217": "2",
+        "CEN315": "1",
+        "CEN316": "2",
+        "CEN416": "1",
+        "CEN417": "2",
+        "CEN516": "1",
+        "CEN517": "2",
+        "CEN116": "1",
+        "CEN218": "1",
+        "CEN219": "2",
+        "CEN221": "3",
+        "CEN317": "1",
+        "CEN318": "2",
+        "CEN322": "3",
+        "CEN418": "1",
+        "CEN419": "2",
+        "CEN420": "3",
+        "CEN518": "1",
+        "CEN519": "2",
+        "CEN520": "3"
+    }
     
     four_weeks = timedelta(days=28)
     features = read_data(DATA_SOURCE_1) + read_data(DATA_SOURCE_2)
@@ -44,6 +97,9 @@ def run():
         # Make the mobile library the first 3 letters of the route
         mobile_library = route[0:3]
         route_number = route[3:6]
+
+        if mobile_library == 'CEN':
+            mobile_library = 'CEN' + CEN_Routes[route]
 
         # Extract stuff from the details
         # e.g. Mobile Library Route Number EDE305 will next visit NORTH ELMHAM, ORCHARD CLOSE No.20 on 18/09/2019 Arrival 16:10 Departure 16:25
