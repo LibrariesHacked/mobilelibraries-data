d72606e525f1f2e9bdd04c2fbfed813b7e85216d
Dave <DaveBathnes@users.noreply.github.com>
2019-12-01 17:09:53 +0000

    #66 East Riding data

--- scripts/east_riding_of_yorkshire.py
+++ scripts/east_riding_of_yorkshire.py
@@ -2,43 +2,87 @@
 
 from urllib.parse import quote
 from datetime import datetime
-import json
 import os
 import csv
-import requests
 import re
 from bs4 import BeautifulSoup
 
-WEBSITE = 'https://www.eastridinglibraries.co.uk/'
-LIBRARY_LIST = 'find-a-library/'
+# HTML obtained using curl
+
+HTML = '../raw/east_riding_of_yorkshire.html'
 DATA_OUTPUT = '../data/east_riding_of_yorkshire.csv'
+
 
 def run():
     """Runs the main script"""
 
     mobiles = []
-    stop_links = []
-    # Get the library list
-    stop_list_html = requests.get(WEBSITE + LIBRARY_LIST)
-    stop_list_soup = BeautifulSoup(stop_list_html.text, 'html.parser')
-    '/find-a-library/?entry=aike_mobile_library '
+    mobile_library = 'Mobile'
 
-    for link_tag in stop_list_soup.find_all('a'):
-        link = link_tag.get('href')
-        if link and '/find-a-library/?entry=' in link and 'mobile_library' in link and link not in stop_links:
-            stop_links.append(link)
+    route_names = {}
 
-    for link in stop_links:
+    route_number = 0
 
-        stop_html = requests.get(WEBSITE + link)
-        stop_soup = BeautifulSoup(stop_html.text, 'html.parser')
+    stop_soup = BeautifulSoup(open(HTML), "html.parser")
 
-        stop_name = stop_soup.find('h1').text.replace('Mobile Library', '').strip()
+    for venue in stop_soup.find_all('div', {"class": "er-filter-block-wrapper"}):
 
-        mobiles.append(
-            [mobile_library, route, community, stop_name, address, postcode, longitude, latitude,
-                day, arrival, departure, 'FREQ=WEEKLY;INTERVAL=4', start, '', timetable]
-        )
+        venue_type = venue.find(
+            'div', {"data-id": "venue_type"}).find('div', {"class": "content"}).get_text()
+
+        if (venue_type) == 'Library_All/Library_Mobile Library':
+
+            stop_name = venue.find('div', {"data-id": "name"}).find(
+                'div', {"class": "content"}).find('a').get_text().replace(' Mobile Library', '')
+            community = stop_name
+            postcode = venue.find(
+                'div', {"data-id": "postcode"}).find('div', {"class": "content"}).get_text()
+            address = stop_name + ', ' + postcode
+            latitude = venue.find(
+                'div', {"data-id": "latitude"}).find('div', {"class": "content"}).get_text()
+            longitude = venue.find(
+                'div', {"data-id": "longitude"}).find('div', {"class": "content"}).get_text()
+            timetable = 'https://www.eastridinglibraries.co.uk' + \
+                venue.find('div', {"data-id": "learn_more"}).find('div',
+                                                                  {"class": "content"}).find('a').get('href').strip()
+
+            openings = venue.find(
+                'div', {"data-id": "opening_times"}).find('div', {"class": "content"}).get_text().split(';')
+
+            opening_times = ''
+            for entry in openings:
+                if '2019' in entry:
+                    opening_times = entry
+
+            times_result = re.search(
+                r'(\d{1,2}\.{0,1}\d{1,2})-(\d{1,2}\.{0,1}\d{1,2})', opening_times)
+            arrival = times_result.group(1).replace('.', ':')
+            if len(arrival) < 3:
+                arrival = arrival + ':00'
+            if (len(arrival.split(':')[1])) < 2:
+                arrival = arrival + '0'
+            departure = times_result.group(2).replace('.', ':')
+            if len(departure) < 3:
+                departure = departure + ':00'
+            if (len(departure.split(':')[1])) < 2:
+                departure = departure + '0'
+            date_result = re.search(r'(2019\d{4})', opening_times)
+            start = datetime.strptime(date_result.group(1), '%Y%m%d')
+            day = start.strftime('%A')
+            start = start.strftime('%Y-%m-%d')
+
+            frequency_result = re.search(r'2019\d{4}(\d)', opening_times)
+            frequency = 'FREQ=WEEKLY;INTERVAL=' + frequency_result.group(1)
+            if not route_names.get(start):
+                route_number = route_number + 1
+                route_names[start] = route_number
+
+            route = route_names[start]
+
+            mobiles.append(
+                [mobile_library, route, community, stop_name, address, postcode, longitude, latitude,
+                    day, arrival, departure, frequency, start, '', timetable]
+            )
 
     with open(DATA_OUTPUT, 'w', encoding='utf8', newline='') as out_csv:
         mob_writer = csv.writer(out_csv, delimiter=',',
